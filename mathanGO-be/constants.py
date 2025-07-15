4b63ab10003d142a0bf50fd0fad45eb12833ccfa
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-15 10:57:48 +0530

    Fix: bind to Render port for deployment

--- mathanGO-be/constants.py
+++ mathanGO-be/constants.py
@@ -2,8 +2,8 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SERVER_URL = 'localhost'
-PORT = '8000'
-ENV = 'dev'
+SERVER_URL = os.getenv("SERVER_URL", "0.0.0.0") 
+PORT = os.getenv("PORT", "8000")              
+ENV = os.getenv("ENV", "dev")
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
